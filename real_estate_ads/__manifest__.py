9cf365e9103279bcc320e99c4fd2bc57d4b70363
mageed1927 <mageed1927@gmail.com>
2026-05-13 00:45:51 +0200

    Add real estate ads module

---
+++ real_estate_ads/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    "name": "Real Estate Ads",
+    "version": "1.0",
+    "website": "https://www.realestate.com",
+    "author": "Misurati",
+    "description": """
+    Real Estate module to show available properties
+    """,
+    "category": "Sales",
+
+    "depends": [],
+
+    "data": [
+        "security/ir.model.access.csv",
+        "views/property_views.xml",
+        "views/menu_items.xml",
+    ],
+
+    "installable": True,
+    "application": True,
+    "license": "LGPL-3",
+}
