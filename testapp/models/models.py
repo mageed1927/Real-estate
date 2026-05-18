d572cdb4252711a7eed4c58117588a46660c7598
mageed1927 <mageed1927@gmail.com>
2026-05-18 15:41:21 +0200

    Fix indentation and update form view

--- testapp/models/models.py
+++ testapp/models/models.py
@@ -1,15 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-from odoo.addons.account_consolidation.report.builder import default
 
 
 class testapp(models.Model):
     _name = 'my.app'
     _description = 'N/A'
-
-    def button_action(self):
-        pass
 
     name = fields.Char(string="My Name", required=True)
     value = fields.Integer(default=30)
@@ -20,14 +16,32 @@ class testapp(models.Model):
     date = fields.Date()
     date_time = fields.Datetime()
     binary = fields.Binary()
-    selection = fields.Selection([('1', 'val1'), ('2', 'val2'), ('3', 'val3'), ('4', 'val4')])
+    selection = fields.Selection([
+        ('1', 'val1'),
+        ('2', 'val2'),
+        ('3', 'val3'),
+        ('4', 'val4')
+    ])
 
     cal1 = fields.Float(string="val 01")
     cal2 = fields.Float(string="val 02")
     result = fields.Float(string="val1 + val2", compute='_value_pc')
+    computed = fields.Float(string="Computed", compute='_compute_value')
 
     @api.depends('cal1', 'cal2')
     def _value_pc(self):
-        self.result = self.cal1 + self.cal2
-     #for record in self:
-     #record.value2 = float(record.value) / 100
+        for record in self:
+            record.result = record.cal1 + record.cal2
+
+    @api.depends('value')
+    def _compute_value(self):
+        for record in self:
+            record.computed = float(record.value) / 100
+
+    def button_action(self):
+        pass
+
+    @api.depends('cal1', 'cal2')
+    def _compute_result(self):
+        for record in self:
+            record.result = (record.cal1 + record.cal2) / 100
