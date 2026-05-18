b41d211525e6a9f98989ad02abaf585af7bdfc24
mageed1927 <mageed1927@gmail.com>
2026-05-18 12:03:54 +0200

    Fix indentation and update form view

--- testapp/models/models.py
+++ testapp/models/models.py
@@ -11,7 +11,7 @@ class testapp(models.Model):
     def button_action(self):
         pass
 
-    name = fields.Char(string="My Name", required=True, placeholder='enter name')
+    name = fields.Char(string="My Name", required=True)
     value = fields.Integer(default=30)
     value2 = fields.Float()
     description = fields.Text(default="ENTER DESCRIPTION")
@@ -22,23 +22,12 @@ class testapp(models.Model):
     binary = fields.Binary()
     selection = fields.Selection([('1', 'val1'), ('2', 'val2'), ('3', 'val3'), ('4', 'val4')])
 
+    cal1 = fields.Float(string="val 01")
+    cal2 = fields.Float(string="val 02")
+    result = fields.Float(string="val1 + val2", compute='_value_pc')
 
-class Myorders(models.Model):
-    _name = 'my.orders'
-    _description = 'My orders'
-    name = fields.Char()
-    date_time = fields.Datetime()
-    items_ids = fields.One2many('my.orders.items', 'order_id', required=True)
-
-
-class MyOrdersitems(models.Model):
-    _name = 'my.orders.items'
-    name = fields.Char()
-    itemPrice = fields.Float()
-    qty = fields.Integer()
-    order_id = fields.Many2one('my.orders')
-
-    # @api.depends('value')
-    # def _value_pc(self):
-    #   for record in self:
-    #       record.value2 = float(record.value) / 100
+    @api.depends('cal1', 'cal2')
+    def _value_pc(self):
+        self.result = self.cal1 + self.cal2
+     #for record in self:
+     #record.value2 = float(record.value) / 100
---
+++ testapp/models/the_orders.py
@@ -0,0 +1,16 @@
+from odoo import models, fields, api
+
+class Myorders(models.Model):
+    _name = 'my.orders'
+    _description = 'My orders'
+    name = fields.Char()
+    date_time = fields.Datetime()
+    items_ids = fields.One2many('my.orders.items', 'order_id', required=True)
+
+class MyOrdersitems(models.Model):
+    _name = 'my.orders.items'
+    name = fields.Char()
+    itemPrice = fields.Float()
+    qty = fields.Integer()
+    order_id = fields.Many2one('my.orders')
+
