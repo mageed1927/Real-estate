585f60fc3acdd261bdcaf1b6352aca6bba89b536
mageed1927 <mageed1927@gmail.com>
2026-05-24 00:48:23 +0200

    Fix indentation and update form view

--- testapp/models/the_orders.py
+++ testapp/models/the_orders.py
@@ -6,11 +6,17 @@ class Myorders(models.Model):
     name = fields.Char()
     date_time = fields.Datetime()
     items_ids = fields.One2many('my.orders.items', 'order_id', required=True)
+    state = fields.Selection(
+        [('new', 'New'), ('ready', 'Ready')], default='new')
+    isActive = fields.Boolean(default=True)
+    def event_ready(self):
+        self.state= 'ready'
+
 
 class MyOrdersitems(models.Model):
     _name = 'my.orders.items'
     name = fields.Char()
     itemPrice = fields.Float()
     qty = fields.Integer()
-    order_id = fields.Many2one('my.orders', domain="[('state','=','ready')]")
+    order_id = fields.Many2one('my.orders', domain="[('state','=','ready'),('isActive','=','True')]")
 
